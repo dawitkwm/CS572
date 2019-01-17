459a826dec9ef16addc6e4a61405f16868ce585f
dawitkwm <dawitkahsay@gmail.com>
2019-01-17 17:40:22 -0600

    completed exercise 1 of homework 04

--- homework-04/check-system.js
+++ homework-04/check-system.js
@@ -1,18 +1,34 @@
-const {Observable} = require('rxjs');
+const {
+    Observable
+} = require('rxjs');
 const os = require('os');
 
+//system info
 let cpuCores = os.cpus();
 let numOfCpuCores = cpuCores.length;
-let ramSize = os.totalmem() / (1024 * 1024 * 1024); 
+let ramSize = os.totalmem() / (1024 * 1024 * 1024);
 
-let checkSystem = Observable.create((observer) => {
-    observer.next({
-        cpuCores, 
-        numOfCpuCores, 
-        ramSize
-    }); //emit info
+let checkSystem = function() {
+    return Observable.create((observer) => {
+        //emit system info
+        observer.next({
+            cpuCores,
+            numOfCpuCores,
+            ramSize
+        });
+    });
+};
+
+checkSystem().subscribe(val => {
+    console.log("Checking your system...");
+    if(val.numOfCpuCores < 2) {
+        console.log("Processor is not supported.");
+    } else if(val.ramSize < 4) {
+        console.log("This app needs at least 4GB of RAM.");
+    } else {
+        console.log("System is checked successfully.");
+        console.log(`Your system has ${val.numOfCpuCores} cores and ${val.ramSize}GB of RAM.`);
+        console.log("The cores are: ");
+        console.log(val.cpuCores)
+    }
 });
-
-checkSystem.subscribe(val => 
-    console.log(val)
-);
